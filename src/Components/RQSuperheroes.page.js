f5470e8d3359b714cc0741d80eaa1d8fcf3275d3
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-09-01 11:50:54 +0300

    Added axios and react-query to the project. Fetched data from http://localhost:4000/superheroes and displayed it on the page.

--- src/Components/RQSuperheroes.page.js
+++ src/Components/RQSuperheroes.page.js
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+const fetchData = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 const RQSuperHeroesPage = () => {
+  const { data, isLoading, isError, error } = useQuery(
+    "super-heroes",
+    fetchData
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
   return (
     <div>
       <h1>RQ Super Heroes</h1>
-      <br />
+      {data.data.map((hero) => (
+        <div key={hero.id}>
+          <p>{hero.name}</p>
+        </div>
+      ))}
     </div>
   );
 };
