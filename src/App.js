4458742e79f8ba10d537c18a0c974a28a56fcc0c
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-09-01 11:49:31 +0300

    react query  config

--- src/App.js
+++ src/App.js
@@ -3,36 +3,44 @@ import SuperHeroesPage from "./Components/Superheroes.page";
 import RQSuperHeroesPage from "./Components/RQSuperheroes.page";
 import HomePage from "./Components/Home.page";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/super-heroes">SuperHeroes</a>
-            </li>
-            <li>
-              <a href="/rq-super-heroes">RQ SuperHeroes</a>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/super-heroes">
-            <SuperHeroesPage />
-          </Route>
-          <Route path="/rq-super-heroes">
-            <RQSuperHeroesPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/super-heroes">SuperHeroes</a>
+              </li>
+              <li>
+                <a href="/rq-super-heroes">RQ SuperHeroes</a>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/super-heroes">
+              <SuperHeroesPage />
+            </Route>
+            <Route path="/rq-super-heroes">
+              <RQSuperHeroesPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 
