f3592706936e41cb34a31db6d8f50542725f5514
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-09-01 11:50:31 +0300

    isloading and error

--- src/Components/Superheroes.page.js
+++ src/Components/Superheroes.page.js
@@ -4,20 +4,32 @@ import { useEffect, useState } from "react";
 const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/superheroes")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error) => setError(error.message));
   }, []);
   if (isLoading) {
     return <div>loading</div>;
   }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
   return (
     <div>
       <h1>Super Heroes</h1>
       {data.map((hero) => (
-        <div key={hero.id}>{hero.name}</div>
+        <div key={hero.id}>
+          <p>{hero.name}</p>
+        </div>
       ))}
     </div>
   );
