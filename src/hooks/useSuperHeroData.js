80ab2c6bc16092f7cb036964af5779a5ac77a3ff
IBRAHIM\Ibrahin <ishaheen910@gmail.com>
2023-09-29 15:16:39 +0300

    Update useSuperHeroData hook to use initialData
    ###Explanation:
    The diff shows that the useSuperHeroData hook is being updated to use the initialData option of the useQuery hook. This option allows you to specify a function that will be called to fetch the initial data for the query. In this case, the function is used to fetch the superhero data from the local query cache. If the data is not found in the cache, the function will fetch it from the server.

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,10 +1,22 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const fetchData = (id) => {
   return axios.get(`http://localhost:4000/superheroes/${id}`);
 };
 
 export const useSuperHeroData = (id) => {
-  return useQuery(["super-hero", id], () => fetchData(id));
+  const queryClient = useQueryClient();
+  return useQuery(["super-hero", id], () => fetchData(id), {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === id);
+      if (hero) {
+        return {
+          data: hero,
+        };
+      } else return undefined;
+    },
+  });
 };
